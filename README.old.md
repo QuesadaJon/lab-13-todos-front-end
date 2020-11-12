c0dc0f7e3b4444a7255d89bae61ab495a30c83f0
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-11-12 08:56:19 -0800

    initial commit

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# lab-13-todos-front-end
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+    Link,
+} from "react-router-dom";
+import Home from './Home.js'
+import Login from './Login.js'
+import SignUp from './SignUp.js'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <ul>
+            <Link to="/login"><div>log in</div></Link>
+            <Link to="/signup"><div>sign up</div></Link>
+          </ul>
+          <Switch>
+            <Route exact path='/' render={(routerProps) => <Home {...routerProps}/> } />
+            <Route exact path='/login' render={(routerProps) => <Login {...routerProps} />} />
+            <Route exact path='/signup' render={(routerProps) => <SignUp {...routerProps}/>} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
